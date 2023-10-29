eeb0211cda503c12bffeea0042d38472f62924c2
Jessmica11 <116199836+Jessmica11@users.noreply.github.com>
2023-10-29 10:29:35 -0400

    trying to pull object of data from API and print it onto HTML area

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,22 @@
-// sample api url
-// https://api.openweathermap.org/data/2.5/weather?q=charlotte&units=imperial&appid=ab11508788fb351f936a398833feadf7
+// target api url example
+// https://api.openweathermap.org/data/2.5/weather?q=Charlotte&units=imperial&callback-test&appid=ab11508788fb351f936a398833feadf7
 
 // global variables
-/*var weather = {
-    "apiKey": "ab11508788fb351f936a398833feadf7"
-}*/
-var apiURL = "http://api.openweathermap.org/data/2.5/weather?q="
-var appID = "&appid="
-var apiKey = "ab11508788fb351f936a398833feadf7" 
+var today = dayjs();
+$('#date').text(today.format("(MM/DD/YYYY)"));
+
+// dayjs formatting
+const dayjs = require('dayjs')
+//import dayjs from 'dayjs' // ES 2015
+dayjs().format()
+
+//to call Current Weather API
+var currentApiURL = "http://api.openweathermap.org/data/2.5/weather?q="
+var appID = "&units=imperial&callback=test&appid="
+var apiKey = "ab11508788fb351f936a398833feadf7"
 
 // local variables
-var userCity = document.querySelector('.user-city').value;
+var userCity = document.querySelector(".user-city").value;
 var formEl = document.querySelector(".search-form")
 
 // Event listener on submit button
@@ -20,40 +26,55 @@ submitButton.addEventListener("click", handleUserSubmit);
 // prevent form from running then immediately clearing
 function handleUserSubmit(event) {
   event.preventDefault();
-
-  var userCity = document.querySelector('.user-city').value;
     var formEl = document.querySelector(".search-form")
-    var queryURL = apiURL.concat(userCity) + appID + apiKey;
+    var currentQueryURL = currentApiURL + userCity + appID + apiKey
 
-  fetch(queryURL)
+  fetch(currentQueryURL)
     .then(function (response) {
     return response.json();
     })
     .then(function (data) {
-    console.log("For the City of: " + userCity);
+    console.log("For City: " + userCity + "," + "USA");
     console.log(data);
   });
 
-// user breakpoint that triggers a modal to pop-up
-    function userInput() {
-        if(userCity = null){
-            function showModal() {
-            var errorModal = document.querySelector(".modal");
-            if (errorModal.style.display === "none") {
-                errorModal.style.display = "block";
-            } else{
-            errorModal.style.display = "none";}     
-            }
-            return;
-        }
+  // I want to be able to pull multiple pieces of data and create an object (?)
+  getWeatherData(data)
+    const  {
+      icon = data.weather.icon,
+      temperature = data.main.temp,
+      wind = data.wind.speed,
+      humidity = data.main.humidtiy,
+      console.log(data);
+    } 
 
-  location.assign(queryString);
-}
+    var icon = data.weather.icon
+    var temperature = data.main.temp
+    var wind = data.wind.speed
+    var humidity = data.main.humidity
 
-// Event listener on submit button
-var submitButton = document.getElementById("submit-button")
-submitButton.addEventListener("click", handleUserSubmit);
+
+    return {
+      icon,
+      temperature,
+      wind,
+      humidtiy,
+    }
+
+    displayCurrentWeather: function (data){
+      const  {
+      icon = data.weather.icon;
+      temperature = data.main.temp;
+      wind = data.wind.speed;
+      humidity = data.main.humidtiy;
+      console.log(getWeatherData)
+
+      document.getElementById("city-name").innerText = userCity
+      document.getElementById("icon").innerText = icon
+      document.getElementById("temperature").innerText = "Temp: " + temperature
+      document.getElementById("wind").innerText = "Wind: " + wind + " MPH"
+      document.getElementById("humidity").innerText = "Humidity: " + humidity + "%"
+
+    }
 
 };
-
-
