327ea726f0348e903e2f2fefebbe00623a9df4c1
Jessmica11 <116199836+Jessmica11@users.noreply.github.com>
2023-10-29 12:48:20 -0400

    Code as written can print specific object properties in the console for any city the user inputs

--- assets/script.js
+++ assets/script.js
@@ -2,22 +2,19 @@
 // https://api.openweathermap.org/data/2.5/weather?q=Charlotte&units=imperial&callback-test&appid=ab11508788fb351f936a398833feadf7
 
 // global variables
+
+// dayjs formatting
 var today = dayjs();
 $('#date').text(today.format("(MM/DD/YYYY)"));
 
-// dayjs formatting
-const dayjs = require('dayjs')
-//import dayjs from 'dayjs' // ES 2015
-dayjs().format()
-
 //to call Current Weather API
 var currentApiURL = "http://api.openweathermap.org/data/2.5/weather?q="
-var appID = "&units=imperial&callback=test&appid="
+var appID = "&units=imperial&appid="
 var apiKey = "ab11508788fb351f936a398833feadf7"
 
-// local variables
+/* local variables
 var userCity = document.querySelector(".user-city").value;
-var formEl = document.querySelector(".search-form")
+var formEl = document.querySelector(".search-form")*/
 
 // Event listener on submit button
 var submitButton = document.getElementById("submit-button")
@@ -26,10 +23,40 @@ submitButton.addEventListener("click", handleUserSubmit);
 // prevent form from running then immediately clearing
 function handleUserSubmit(event) {
   event.preventDefault();
+    var userCity = document.querySelector(".user-city").value;
     var formEl = document.querySelector(".search-form")
-    var currentQueryURL = currentApiURL + userCity + appID + apiKey
+    var currentQueryURL = currentApiURL + userCity + appID + apiKey;
+    fetch(currentQueryURL)
+    .then(function (response) {
+    return response.json();
+    })
+    .then(function (data) {
+    console.log("For City: " + userCity + "," + " USA");
 
-  fetch(currentQueryURL)
+    var weatherIcon = data.weather[0].icon
+    var temperature = data.main.temp
+    var wind = data.wind.speed
+    var humidity = data.main.humidity
+
+    function displayCurrentWeather(data){
+      document.getElementById("city-name").innerText = userCity
+      document.getElementById("icon").innerText = weatherIcon
+      document.getElementById("temperature").innerText = temperature
+      document.getElementById("wind").innerText = wind + " MPH"
+      document.getElementById("humidity").innerText = humidity + "%"
+
+    }
+
+    console.log(temperature, wind, humidity, weatherIcon);
+  });
+    //console.log(currentQueryURL.main.temp);
+}
+
+  function userSubmit() {
+    this.fetchUserSubmit(document.querySelector("#submit-button").value);
+  }
+
+  /*fetch(currentQueryURL)
     .then(function (response) {
     return response.json();
     })
@@ -39,42 +66,23 @@ function handleUserSubmit(event) {
   });
 
   // I want to be able to pull multiple pieces of data and create an object (?)
-  getWeatherData(data)
-    const  {
-      icon = data.weather.icon,
-      temperature = data.main.temp,
-      wind = data.wind.speed,
-      humidity = data.main.humidtiy,
-      console.log(data);
-    } 
+    var weatherIcon = response.data.weather[0].icon
+    var temperature = response.data.main.temp
+    var wind = response.data.wind.speed
+    var humidity = reseponse.data.main.humidity
 
-    var icon = data.weather.icon
-    var temperature = data.main.temp
-    var wind = data.wind.speed
-    var humidity = data.main.humidity
-
-
-    return {
-      icon,
-      temperature,
-      wind,
-      humidtiy,
+  /*function getWeatherData(){
+    
+    
+    const temperature = {
+      response.data.main.temp
     }
 
-    displayCurrentWeather: function (data){
-      const  {
-      icon = data.weather.icon;
-      temperature = data.main.temp;
-      wind = data.wind.speed;
-      humidity = data.main.humidtiy;
-      console.log(getWeatherData)
-
+    function displayCurrentWeather(data){
       document.getElementById("city-name").innerText = userCity
-      document.getElementById("icon").innerText = icon
+      document.getElementById("icon").innerText = weatherIcon
       document.getElementById("temperature").innerText = "Temp: " + temperature
       document.getElementById("wind").innerText = "Wind: " + wind + " MPH"
       document.getElementById("humidity").innerText = "Humidity: " + humidity + "%"
 
-    }
-
-};
+    }*/
